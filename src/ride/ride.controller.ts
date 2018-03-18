08ba0560d7be9d5c31d921107b421fa137e37857
omri <omrishtam@gmail.com>
2018-03-18 09:40:27 +0200

    Controllers mostly implemented.

--- src/ride/ride.controller.ts
+++ src/ride/ride.controller.ts
@@ -1,7 +1,21 @@
 import * as mongoose from 'mongoose';
 import { rideRepository } from './ride.repository';
+import { IRide } from './ride.interface';
 
 export class rideController {
+
+  static getAllRides() {
+    return rideRepository.getAll(undefined, undefined, 'riders');
+  }
+
+  static addRide(ride: IRide) {
+    return rideRepository.save(ride);
+  }
+
+  static deleteRide(id: mongoose.Types.ObjectId) {
+    return rideRepository.deleteById(id);
+  }
+
   static addRider(rideid: mongoose.Types.ObjectId, userid: string) {
     return rideRepository.updateById(rideid,
                                      { $push: { riders: userid } },
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,8 +3,21 @@ import { userRepository } from './user.repository';
 import { INotification } from '../notification/notification.interface';
 
 export class userController {
+
   static getAllUsers() {
     return userRepository.getAll(undefined, undefined, 'rides.ride');
+  }
+
+  static getUser(id: string) {
+    return userRepository.getOneByProps({ _id: id }, 'rides.ride');
+  }
+  
+  static async getUserActiveRides(id: string) {
+    const userRides = await userRepository.getOneByProps({ _id: id },
+      { path: 'rides.ride', model: 'Ride', match: { active: true } });
+    if (userRides) {
+      return userRides.rides.filter((e) => { return e.ride != null; });
+    }
   }
 
   static pushNotification(id: string, notificaiton: INotification) {
@@ -19,11 +32,11 @@ export class userController {
     return userRepository.updateById(id, { $pull: { rides: { ride } } });
   }
 
-  static async getActiveRides(id: string) {
-    const userRides = await userRepository.getOneByProps({ _id: id },
-      { path: 'rides.ride', model: 'Ride', match: { active: true } }, '-_id rides');
-    if (userRides) {
-      return userRides.rides.filter((e) => { return e.ride != null; });
-    }
+  static getNotifications(id: string) {
+    return userRepository.getOneByProps({ _id: id }, undefined, 'notifications');
+  }
+
+  static markNotificationRead(id: string, notificaiton: mongoose.Types.ObjectId) {
+    return userRepository.updateById(id, {});
   }
 }
