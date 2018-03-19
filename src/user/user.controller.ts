053562db6bc3455388301db4e6245b025eb61439
omri <omrishtam@gmail.com>
2018-03-19 08:55:34 +0200

    Added update, save, delete functions.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,6 +2,7 @@ import * as mongoose from 'mongoose';
 import { userRepository } from './user.repository';
 import { INotification } from '../notification/notification.interface';
 import { IRide } from '../ride/ride.interface';
+import { IUser } from './user.interface';
 
 export class userController {
 
@@ -11,6 +12,18 @@ export class userController {
 
   static getUser(id: string) {
     return userRepository.getOneByProps({ _id: id, isDeleted: false });
+  }
+
+  static save(user: IUser) {
+    return userRepository.save(user);
+  }
+
+  static updateById(id: string, update: any) {
+    return userRepository.updateById(id, update);
+  }
+
+  static deleteById(id: string) {
+    return userRepository.deleteById(id);
   }
 
   static pushNotification(id: string, notificaiton: INotification) {
