f37be7ce1b093deb7abb2b8b2163ec8958a03842
Omri Shtamberger <omrishtam@gmail.com>
2018-04-01 15:11:03 +0300

    Added comments to each ride service function.

--- src/ride/ride.service.ts
+++ src/ride/ride.service.ts
@@ -4,6 +4,12 @@ import { Ride } from './ride.model';
 
 export class rideService {
 
+  /**
+   * Returns all rides.
+   * @param conditions Query conditions
+   * @param populate Paths to populate
+   * @param select Paths to select
+   */
   static getAll(conditions?: any, populate?: any, select?: string) {
     let rides = Ride.find(conditions || {}, select || {});
     if (populate) {
@@ -13,6 +19,12 @@ export class rideService {
     return rides;
   }
 
+  /**
+   * Returns a single ride.
+   * @param conditions Query conditions
+   * @param populate Paths to populate
+   * @param select Paths to select
+   */
   static getOneByProps(conditions: any, populate?: any, select?: string) {
     let ride = Ride.findOne(conditions, select || {});
     if (populate) {
@@ -22,14 +34,28 @@ export class rideService {
     return ride;
   }
 
+  /**
+   * Creates a ride.
+   * @param ride Ride to create
+   */
   static create(ride: IRide) {
     return ride.save();
   }
 
+  /**
+   * Marks a ride as deleted.
+   * @param id Ride id
+   */
   static deleteById(id: Types.ObjectId) {
     return this.updateById(id, { isDeleted: true });
   }
 
+  /**
+   * Updates ride details by id.
+   * @param id Ride id
+   * @param update Updated ride
+   * @param populate Paths to populate
+   */
   static updateById(id: Types.ObjectId, update: any, populate?: any) {
     let ride = Ride.findByIdAndUpdate(id, update, { new: true });
     if (populate) {
