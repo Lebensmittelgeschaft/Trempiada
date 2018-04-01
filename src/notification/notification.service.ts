4f7cb236e64c0a3437f1645e81bcfa7f7502567c
Omri Shtamberger <omrishtam@gmail.com>
2018-04-01 15:16:52 +0300

    Added comments to notification service functions.

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -4,6 +4,13 @@ import { Notification } from './notification.model';
 import { User } from '../user/user.model';
 
 export class notificationService {
+
+  /**
+   * Returns all notifications.
+   * @param conditions Query conditions
+   * @param populate Paths to populate
+   * @param select Paths to select
+   */
   static getAll(conditions?: any, populate?: any, select?: string) {
     if (populate) {
       return Notification.find(conditions || {}, select || {}).populate(populate);
@@ -12,6 +19,12 @@ export class notificationService {
     return Notification.find(conditions || {}, select || {});
   }
 
+  /**
+   * Returns a single notification.
+   * @param conditions Query conditions
+   * @param populate Paths to populate
+   * @param select Paths to select
+   */
   static getOneByProps(conditions: any, populate?: any, select?: string) {
     if (populate) {
       return Notification.findOne(conditions, select || {}).populate(populate);
@@ -20,18 +33,29 @@ export class notificationService {
     return Notification.findOne(conditions, select || {});
   }
 
+  /**
+   * Creates a notification.
+   * @param notification Notification to create
+   */
   static create(notification: INotification) {
     return notification.save();
   }
 
-  static deleteById(id: Types.ObjectId, populate?: any) {
-    if (populate) {
-      return Notification.findByIdAndRemove(id).populate(populate);
-    }
+  /**
+   * Marks a notification as deleted.
+   * @param id Notification id
+   */
+  static deleteById(id: Types.ObjectId) {
 
     return Notification.findByIdAndRemove(id);
   }
 
+   /**
+   * Updates notification details by id.
+   * @param id Notification id
+   * @param update Updated notification
+   * @param populate Paths to populate
+   */
   static updateById(id: Types.ObjectId, update: any, populate?: any) {
     if (populate) {
       return Notification.findByIdAndUpdate(id, update, { new: true }).populate(populate);
