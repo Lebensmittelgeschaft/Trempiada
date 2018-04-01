be7dbe0180074ac2d6272d5ed5b0397fa0c1066e
Omri Shtamberger <omrishtam@gmail.com>
2018-04-01 15:14:21 +0300

    Added comments to user service functions.

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,22 +3,37 @@ import { IUser } from './user.interface';
 
 export class userService {
   
+  /**
+   * Returns all users.
+   * @param conditions Query conditions
+   * @param select Paths to select
+   */
   static getAll(conditions?: any, select?: string) {
     return User.find(conditions || {}, select || {});
   }
 
+  /**
+   * Returns a single user.
+   * @param conditions Query conditions
+   * @param select Paths to select
+   */
   static getOneByProps(conditions: any, select?: string) {
     return User.findOne(conditions, select || {});
   }
 
+  /**
+   * Creates a user.
+   * @param user User to create
+   */
   static create(user: IUser) {
     return user.save();
   }
 
-  static deleteById(id: string) {
-    return User.findByIdAndRemove(id);
-  }
-
+  /**
+   * Updates user details by id.
+   * @param id User id
+   * @param update Updated user
+   */
   static updateById(id: string, update: any) {
     return User.findByIdAndUpdate(id, update, { new: true });
   }
